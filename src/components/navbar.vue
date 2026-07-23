05d31e46a3436cb92661587fdc607f5dd40bb10e
GeneTouffu <130595907+GeneTouffu@users.noreply.github.com>
2026-07-23 18:31:47 +1000

    navbar

---
+++ src/components/navbar.vue
@@ -0,0 +1,17 @@
+<template>
+  <nav class="navbar">
+    <a class="brand" href="#">danbutres.dev</a>
+    <div class="nav-links">
+      <a href="#">Home</a>
+      <a href="#blogs">Blogs</a>
+      <a href="#about">About</a>
+      <a href="#contact">Contact</a>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Navbar'
+}
+</script>
