2d7584f97f0ddcff03e4308311ece02f3eaf7eb7
GeneTouffu <GeneTouffu@users.noreply.github.com>
2026-08-13 08:02:25 +0000

    deploy: 399a949593e0dab0d35808c48418e124b8387597

---
+++ assets/test-two-D-t5ldTn.js
@@ -0,0 +1,15 @@
+var e=`---
+title: "Test Two?"
+date: 2026-08-13
+category: Not Bgs
+tags:
+
+slug: not-bgs/test-two
+---
+
+# Test Two?
+
+Write your blog post here.
+
+# References
+`;export{e as default};
